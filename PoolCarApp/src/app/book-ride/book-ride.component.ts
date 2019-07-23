cd782695540f92dd00fda0bffc8df0bc995063a3
Sanghamitra1234 <sanghota4567@gmail.com>
2019-07-23 22:40:04 +0530

    pipes added for ride filters

--- PoolCarApp/src/app/book-ride/book-ride.component.ts
+++ PoolCarApp/src/app/book-ride/book-ride.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {RideFiltersPipe} from './ride-filters.pipe';
 
 @Component({
   selector: 'app-book-ride',
   templateUrl: './book-ride.component.html',
-  styleUrls: ['./book-ride.component.css']
+  styleUrls: ['./book-ride.component.css'],
+  providers:[RideFiltersPipe]
 })
 export class BookRideComponent implements OnInit {
   clicked:boolean=true;
-  rideDetails=[{id:1,"offerId":"ABC","name":"sang","car":"mercedes","seatsLeft":2,"pickUp":"Gotham","destination":"PTP"},
-              {id:2,"offerId":"ABCD","name":"John","car":"mercedesBenz","seatsLeft":3,"pickUp":"PTP","destination":"Gotham"},
-              {id:3,"offerId":"AB","name":"Doe","car":"Swift","seatsLeft":2,"pickUp":"VaniVihar","destination":"Gotham"},
-              {id:4,"offerId":"ABD","name":"Dove","car":"Fasicno","seatsLeft":4,"pickUp":"VaniVihar","destination":"AcharyaVihar"}]
-  constructor() { }
+  constructor(private rideFilters: RideFiltersPipe) {}
+  rideDetails=[{id:1,offerId:"ABC",name:"sang",car:"mercedes",seatsLeft:2,pickUp:"Gotham",destination:"PTP"},
+              {id:2,offerId:"ABCD",name:"John",car:"mercedesBenz",seatsLeft:3,pickUp:"PTP",destination:"Gotham"},
+              {id:3,offerId:"AB",name:"Doe",car:"Swift",seatsLeft:2,pickUp:"VaniVihar",destination:"Gotham"},
+              {id:4,offerId:"ABD",name:"Dove",car:"Fasicno",seatsLeft:4,pickUp:"VaniVihar",destination:"AcharyaVihar"}]
+
+  temp=this.rideDetails;
+  
+  rideNew=[];
+  changeTableWithEndToStart(params){
+    console.log(typeof this.rideDetails, this.rideDetails.length ,this.rideDetails);
+    this.rideNew=this.rideFilters.transform(this.rideDetails,params);
+      if(this.rideNew){
+        this.rideDetails=this.rideNew;
+      }
+   }
+  
+   
+  
+  
 
   ngOnInit() {
   }
--- PoolCarApp/src/app/book-ride/ride-filters.pipe.ts
+++ PoolCarApp/src/app/book-ride/ride-filters.pipe.ts
@@ -5,8 +5,34 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class RideFiltersPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: object[], args: string): object[] {
+    let rideNewArray=[];
+    if(args==='to'){
+      console.log("inside To" ,value[1]['destination']);
+      for(let i=0;i<value.length;i++){
+        if(value[i]['destination']=='Gotham'){
+          console.log("inside To Loop");
+          rideNewArray.push(value[i]);
+        }
+      }
+      return rideNewArray;
+    }
+    else if(args==='from'){
+      for(let i=0;i<value.length;i++){
+        if(value[i]['pickUp']=='Gotham'){
+          rideNewArray.push(value[i]);
+        }
+      }
+      return rideNewArray;
+    }
+    else if(args==='others'){
+      for(let i=0;i<value.length;i++){
+        if(value[i]['pickUp']!='Gotham' && value[i]['destination']!='Gotham'){
+          rideNewArray.push(value[i]);
+        }
+      }
+      return rideNewArray;
+    }
   }
 
 }
